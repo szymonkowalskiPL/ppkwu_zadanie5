dce35546a00c45ebdf9b41cc0aefdc195b67c138
szkowalski <222477@edu.p.lodz.pl>
2021-12-09 12:49:10 +0100

    server mistake fixed

--- rest-api.py
+++ rest-api.py
@@ -2,14 +2,12 @@ from flask import Flask, request, jsonify
 import re
 import requests
 import json
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-@app.route('/find_company/<string>')
-def find(string):
-    html = requests.get("https://panoramafirm.pl/szukaj?k={keyword}&l=".format(keyword=key_word)).text
-    soup = BeautifulSoup(html, features="html.parser")
+@app.route('/findCompany/<word>')
+def checkString(word):
+    print(word)
+    return "OK"
 
 app.run(host="localhost", port=8001, debug=False)
-
