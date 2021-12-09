0d7a7c4075223fb1f659ba640b3d65f3f2ff6a91
szkowalski <222477@edu.p.lodz.pl>
2021-12-09 12:52:01 +0100

    data from panorama firm receiced

--- rest-api.py
+++ rest-api.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, jsonify
-import re
+from flask import Flask, Response, render_template
+from bs4 import BeautifulSoup
 import requests
-import json
+import vobject
+
+database = []
 
 app = Flask(__name__)
 
-@app.route('/findCompany/<word>')
+@app.route('/find/<word>')
 def checkString(word):
-    print(word)
+    html = requests.get("https://panoramafirm.pl/szukaj?k={keyword}&l=".format(keyword=word)).text
+    soup = BeautifulSoup(html, features="html.parser")
+    lst = soup.find('ul', {"id": "company-list"})
     return "OK"
 
 app.run(host="localhost", port=8001, debug=False)
